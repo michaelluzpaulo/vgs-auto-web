f9f6d7001ab1a0037931a82746e004d750ed5480
jossana luz <jossana.paulo@gmail.com>
2024-11-07 17:17:33 -0300

    executado yarn build

--- Modules/Site/Resources/views/carro.blade.php
+++ Modules/Site/Resources/views/carro.blade.php
@@ -1,6 +1,13 @@
 @extends('site::layouts.master')
 @section('js')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+    <script>
+        @if ($carro->vendido == 'S')
+            document.querySelector('.nav-link-2').classList.add("active");
+        @else
+            document.querySelector('.nav-link-1').classList.add("active");
+        @endif
+    </script>
 @endsection
 @section('css')
     <link href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" rel="stylesheet" />
@@ -23,11 +30,11 @@
             </div>
         </div>
 
-        <div class="p-geral pg-internas content-artigo">
+        <div class="p-geral">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6">
-                        <figure class="zommcontainer mt-3 ">
+                        <figure class="zommcontainer">
                             <img src="/storage/carro/big_{{ $carro->img }}" class="modify-img-principal"
                                 style="width: 100%">
                         </figure>
@@ -37,7 +44,7 @@
                                 @if (count($carroFotos) > 0)
                                     @foreach ($carroFotos as $foto)
                                         <div class=" col-lg-3 col-md-6 col-sm-4 p-2">
-                                            <figure class="thumb-selector g-transparencia">
+                                            <figure class="thumb-selector">
                                                 <a href="/storage/carro/big_{{ $foto->img }}" title="<?php echo $foto['legenda']; ?>"
                                                     style="display: block" class="galeria-img amplia-imagem">
                                                     <img src="/storage/carro/tmb_{{ $foto->img }}" />
@@ -86,18 +93,7 @@
                                     </tr>
                                 </table>
                             </div>
-                            {!! $carro->texto !!}
-                        </div>
-                        <div class="atendimento-whatsapp pt-4 text-center">
-                            <img src="/img/site/atendimento-whats.png" title="Atendimento" />
-                            <br />
-                            <a target="_blank"
-                                href="https://api.whatsapp.com/send?phone=5551981640439&text=Ol%C3%A1%2C%20visitei%20o%20site%20da%20Auto%20Top%20Multimarcas%20e%20gostaria%20de%20receber%20mais%20informa%C3%A7%C3%B5es"
-                                class="telefone text-dark">(51) 98164 0439</a>
-                            <br />
-                            <a target="_blank"
-                                href="https://api.whatsapp.com/send?phone=5551999260123&text=Ol%C3%A1%2C%20visitei%20o%20site%20da%20Auto%20Top%20Multimarcas%20e%20gostaria%20de%20receber%20mais%20informa%C3%A7%C3%B5es"
-                                class="telefone text-dark">(51) 99926 0123</a>
+                            <p>{!! $carro->texto !!}</p>
                         </div>
                     </div>
 
--- storage/framework/views/9691be92d7aa6b2acd3533bb77f453e8.php
+++ storage/framework/views/9691be92d7aa6b2acd3533bb77f453e8.php
@@ -1,5 +1,12 @@
 <?php $__env->startSection('js'); ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+    <script>
+        <?php if($carro->vendido == 'S'): ?>
+            document.querySelector('.nav-link-2').classList.add("active");
+        <?php else: ?>
+            document.querySelector('.nav-link-1').classList.add("active");
+        <?php endif; ?>
+    </script>
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('css'); ?>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" rel="stylesheet" />
@@ -22,11 +29,11 @@
             </div>
         </div>
 
-        <div class="p-geral pg-internas content-artigo">
+        <div class="p-geral">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6">
-                        <figure class="zommcontainer mt-3 ">
+                        <figure class="zommcontainer">
                             <img src="/storage/carro/big_<?php echo e($carro->img); ?>" class="modify-img-principal"
                                 style="width: 100%">
                         </figure>
@@ -36,7 +43,7 @@
                                 <?php if(count($carroFotos) > 0): ?>
                                     <?php $__currentLoopData = $carroFotos; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $foto): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                                         <div class=" col-lg-3 col-md-6 col-sm-4 p-2">
-                                            <figure class="thumb-selector g-transparencia">
+                                            <figure class="thumb-selector">
                                                 <a href="/storage/carro/big_<?php echo e($foto->img); ?>" title="<?php echo $foto['legenda']; ?>"
                                                     style="display: block" class="galeria-img amplia-imagem">
                                                     <img src="/storage/carro/tmb_<?php echo e($foto->img); ?>" />
@@ -85,19 +92,7 @@
                                     </tr>
                                 </table>
                             </div>
-                            <?php echo $carro->texto; ?>
-
-                        </div>
-                        <div class="atendimento-whatsapp pt-4 text-center">
-                            <img src="/img/site/atendimento-whats.png" title="Atendimento" />
-                            <br />
-                            <a target="_blank"
-                                href="https://api.whatsapp.com/send?phone=5551981640439&text=Ol%C3%A1%2C%20visitei%20o%20site%20da%20Auto%20Top%20Multimarcas%20e%20gostaria%20de%20receber%20mais%20informa%C3%A7%C3%B5es"
-                                class="telefone text-dark">(51) 98164 0439</a>
-                            <br />
-                            <a target="_blank"
-                                href="https://api.whatsapp.com/send?phone=5551999260123&text=Ol%C3%A1%2C%20visitei%20o%20site%20da%20Auto%20Top%20Multimarcas%20e%20gostaria%20de%20receber%20mais%20informa%C3%A7%C3%B5es"
-                                class="telefone text-dark">(51) 99926 0123</a>
+                            <p><?php echo $carro->texto; ?></p>
                         </div>
                     </div>
 
