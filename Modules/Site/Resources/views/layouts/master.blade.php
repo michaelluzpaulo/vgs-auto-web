2870738a69089f301885c99142c97ddf15aae9a2
jossana luz <jossana.paulo@gmail.com>
2024-11-12 11:55:56 -0300

    ajuste btn mobile home

--- Modules/Site/Resources/views/layouts/master.blade.php
+++ Modules/Site/Resources/views/layouts/master.blade.php
@@ -81,7 +81,7 @@
                                         alt="logo VGS negócios automotivos">
                                 </div>
                             </a>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                                 aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
